2ad8f7561734774928a2abf0323c05b420f94367
adrielhenry33 <adrielhenrydb@gmail.com>
2025-09-04 11:02:48 -0300

    feat : app bar criado

--- lib/app/pages/login/login_page.dart
+++ lib/app/pages/login/login_page.dart
@@ -20,153 +20,163 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final appController = Modular.get<AppController>();
 
-    return Scaffold(
-      backgroundColor: appController.isDark ? Colors.white : Colors.black87,
-      appBar: AppBar(
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: appController.isDark ? Colors.white : Colors.black87,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.shopping_cart,
-                  size: 50,
-                  color: Colors.deepOrangeAccent,
-                  fill: 1.0,
+        appBar: AppBar(
+          backgroundColor: appController.isDark ? Colors.white : Colors.black87,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.shopping_cart,
+                    size: 50,
+                    color: Colors.deepOrangeAccent,
+                    fill: 1.0,
+                  ),
+
+                  SizedBox(width: 8),
+
+                  Text(
+                    'FakeStore',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: appController.isDark
+                          ? Colors.black87
+                          : Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Image(image: AssetImage('images/login.jpg')),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email_outlined, size: 20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  label: Text(
+                    'email',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(16),
                 ),
+              ),
 
-                SizedBox(width: 8),
+              SizedBox(height: 15),
 
-                Text(
-                  'FakeStore',
-                  style: GoogleFonts.montserrat(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
+              TextField(
+                obscureText: !_isVisiblePass,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock_outline, size: 20),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _isVisiblePass = !_isVisiblePass;
+                      });
+                    },
+                    icon: Icon(
+                      _isVisiblePass
+                          ? Icons.visibility_off_outlined
+                          : Icons.visibility,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  label: Text(
+                    'password',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(16),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  Modular.to.pushReplacementNamed('/home');
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 2,
+                  backgroundColor: Colors.deepOrangeAccent,
+                  padding: EdgeInsets.all(16),
+                  textStyle: TextStyle(color: Colors.white),
+                  minimumSize: Size.fromHeight(55),
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(12),
+                  ),
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+
+              SizedBox(height: 15),
+
+              ElevatedButton(
+                onPressed: () {
+                  Modular.to.pushNamed('/register');
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 2,
+                  backgroundColor: Colors.deepOrangeAccent,
+                  padding: EdgeInsets.all(16),
+                  textStyle: TextStyle(color: Colors.white),
+                  minimumSize: Size.fromHeight(55),
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(12),
+                  ),
+                ),
+                child: Text(
+                  'Criar conta ',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+
+              SizedBox(height: 15),
+
+              TextButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  padding: EdgeInsets.all(16),
+                  textStyle: TextStyle(color: Colors.white),
+                  minimumSize: Size.fromHeight(55),
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(12),
+                  ),
+                ),
+                child: Text(
+                  'Esqueceu sua senha? ',
+                  style: TextStyle(
                     color: appController.isDark ? Colors.black87 : Colors.white,
+                    fontSize: 16,
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 15),
-            Image(image: AssetImage('images/login.jpg')),
-            SizedBox(height: 20),
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email_outlined, size: 20),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                label: Text(
-                  'email',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-                ),
-                contentPadding: EdgeInsets.all(16),
               ),
-            ),
-
-            SizedBox(height: 15),
-
-            TextField(
-              obscureText: !_isVisiblePass,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.lock_outline, size: 20),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _isVisiblePass = !_isVisiblePass;
-                    });
-                  },
-                  icon: Icon(
-                    _isVisiblePass
-                        ? Icons.visibility_off_outlined
-                        : Icons.visibility,
-                  ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                label: Text(
-                  'password',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-                ),
-                contentPadding: EdgeInsets.all(16),
-              ),
-            ),
-
-            SizedBox(height: 20),
-
-            ElevatedButton(
-              onPressed: () {
-                Modular.to.pushReplacementNamed('/home');
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 2,
-                backgroundColor: Colors.deepOrangeAccent,
-                padding: EdgeInsets.all(16),
-                textStyle: TextStyle(color: Colors.white),
-                minimumSize: Size.fromHeight(55),
-
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(12),
-                ),
-              ),
-              child: Text(
-                'Login',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-            ),
-
-            SizedBox(height: 15),
-
-             ElevatedButton(
-              onPressed: () {
-                Modular.to.navigate('/register');
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 2,
-                backgroundColor: Colors.deepOrangeAccent,
-                padding: EdgeInsets.all(16),
-                textStyle: TextStyle(color: Colors.white),
-                minimumSize: Size.fromHeight(55),
-
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(12),
-                ),
-              ),
-              child: Text(
-                'Criar conta ',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-            ),
-
-            SizedBox(height: 15),
-
-            TextButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                padding: EdgeInsets.all(16),
-                textStyle: TextStyle(color: Colors.white),
-                minimumSize: Size.fromHeight(55),
-
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(12),
-                ),
-              ),
-              child: Text(
-                'Esqueceu sua senha? ',
-                style: TextStyle(
-                  color: appController.isDark ? Colors.black87 : Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/app/pages/registration/registration_page.dart
+++ lib/app/pages/registration/registration_page.dart
@@ -1,4 +1,7 @@
+import 'package:arq_app/controllers/app_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({super.key});
@@ -10,6 +13,38 @@ class RegistrationPage extends StatefulWidget {
 class _RegistrationPageState extends State<RegistrationPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final appController = Modular.get<AppController>();
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'FakeStore',
+            style: GoogleFonts.montserrat(
+              textStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: appController.isDark ? Colors.black : Colors.white,
+              ),
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrangeAccent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.vertical(
+              top: Radius.circular(16),
+            ),
+          ),
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
