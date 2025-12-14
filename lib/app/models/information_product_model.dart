f7915c562ef40ace03f5d130f9ec146fdf07b1ac
adrielhenry33 <adrielhenrydb@gmail.com>
2025-12-14 20:53:14 -0300

    feat: cria os providers para serem utilizados na exibição de categorias

--- lib/app/models/information_product_model.dart
+++ lib/app/models/information_product_model.dart
@@ -10,3 +10,4 @@ class InformationProductModel {
     required this.shippingInformation,
   });
 }
+
---
+++ lib/providers/categorias_providers.dart
@@ -0,0 +1,15 @@
+import 'package:arq_app/app/interfaces/product_category_interface.dart';
+import 'package:arq_app/app/models/product_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+final produtsRepositoryyProvider = Provider<IProductCategoryInterface>((ref) {
+  final lista = Modular.get<IProductCategoryInterface>();
+  return lista;
+});
+
+final produtsByCategory = FutureProvider.family
+    .autoDispose<List<ProductModel>, String>((ref, categoria) async {
+      final lista = ref.watch(produtsRepositoryyProvider);
+      return await lista.getCategory(categoria);
+    });
