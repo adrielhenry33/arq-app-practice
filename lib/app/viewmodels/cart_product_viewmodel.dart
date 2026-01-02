2facaf251a5407045f707cb7625d774fcdb09e15
adrielhenry33 <adrielhenrydb@gmail.com>
2026-01-02 18:18:15 -0300

    refactor: refatora a cartviewmodel para receber objeto unico ao inves de varios produtos

--- lib/app/viewmodels/cart_product_viewmodel.dart
+++ lib/app/viewmodels/cart_product_viewmodel.dart
@@ -1,39 +1,76 @@
-import 'package:arq_app/app/models/product_model.dart';
+import 'package:arq_app/app/models/cart_product_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CartProductViewmodel extends StateNotifier<List<ProductModel>> {
+class CartProductViewmodel extends StateNotifier<List<CartProductModel>> {
   CartProductViewmodel() : super([]);
 
-  void addProduct(ProductModel produto) {
-    state = [...state, produto];
+  void add(CartProductModel produto) {
+    final exists = state.any((p) => p.produto.id == produto.produto.id);
+
+    if (exists) {
+      final ocurrence = state.indexWhere(
+        (p) => p.produto.id == produto.produto.id,
+      );
+
+      List<CartProductModel> listAux = [...state];
+
+      final copia = listAux[ocurrence].copyWith(
+        quantidade: listAux[ocurrence].quantidade + 1,
+      );
+
+      listAux[ocurrence] = copia;
+      state = listAux;
+    } else {
+      state = [...state, produto.copyWith(quantidade: 1)];
+    }
   }
 
-  void removeProduct(ProductModel produto) {
+  void delete(CartProductModel produto) {
     final exists = state.any((p) {
-      return p.id == produto.id;
+      return p.produto.id == produto.produto.id;
     });
 
     if (exists) {
-      final firstOcurrence = state.indexWhere((p) {
-        return p.id == produto.id;
-      });
-      List<ProductModel> listAux = [...state];
-      listAux.removeAt(firstOcurrence);
-      state = listAux;
+      final ocurrence = state.indexWhere(
+        (p) => p.produto.id == produto.produto.id,
+      );
+
+      List<CartProductModel> listAux = [...state];
+
+      final copia = listAux[ocurrence].copyWith(
+        quantidade: listAux[ocurrence].quantidade - 1,
+      );
+
+      if (copia.quantidade < 1) {
+        listAux.removeAt(ocurrence);
+        state = listAux;
+      } else {
+        listAux[ocurrence] = copia;
+        state = listAux;
+      }
+    }
+  }
+
+  void deleteAll() {
+    if (state.isNotEmpty) {
+      state = [];
     }
   }
 }
 
 //cerebro           //o que sera retornado
 final cartProvider =
-    StateNotifierProvider<CartProductViewmodel, List<ProductModel>>(
+    StateNotifierProvider<CartProductViewmodel, List<CartProductModel>>(
       (ref) => CartProductViewmodel(),
     );
 
 final findOcurrenceProvider = Provider.family<int, int>((ref, id) {
   final provider = ref.watch(cartProvider);
-  final ocurrence = provider.where((p) {
-    return p.id == id;
-  });
-  return ocurrence.length;
+
+  try {
+    final ocurrence = provider.firstWhere((p) => p.produto.id == id);
+    return ocurrence.quantidade;
+  } catch (e) {
+    return 0;
+  }
 });
