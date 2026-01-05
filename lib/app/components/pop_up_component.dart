8ca6d120783f6dd5b5f3e48f0dd66b76e4b57eea
adrielhenry33 <adrielhenrydb@gmail.com>
2026-01-04 21:19:21 -0300

    feat: cria a funcioalidade de compartilhar o produto de acordo com a preferencia do usuario

--- lib/app/components/pop_up_component.dart
+++ lib/app/components/pop_up_component.dart
@@ -1,7 +1,11 @@
+import 'package:arq_app/app/models/product_model.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class PopUpComponent extends StatefulWidget {
-  const PopUpComponent({super.key});
+  final ProductModel produto;
+
+  const PopUpComponent({super.key, required this.produto});
 
   @override
   State<PopUpComponent> createState() => _PopUpComponentState();
@@ -12,12 +16,14 @@ class _PopUpComponentState extends State<PopUpComponent> {
   Widget build(BuildContext context) {
     return PopupMenuButton(
       icon: Icon(Icons.more_vert),
-      onSelected: (value) {
-        if (value == 'compartilhar') {
+      onSelected: (value) async {
+        if (value == 'Compartilhar') {
+          await _shareInfo();
         } else if (value == 'Ver detalhes') {}
       },
       itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
         PopupMenuItem(
+          value: 'Compartilhar',
           child: ListTile(
             leading: Icon(Icons.share, color: Colors.black54),
             title: Text('Compartilhar'),
@@ -26,6 +32,7 @@ class _PopUpComponentState extends State<PopUpComponent> {
         ),
 
         PopupMenuItem(
+          value: 'Ver detalhes',
           child: ListTile(
             leading: Icon(Icons.info_outline, color: Colors.black54),
             title: Text('Ver detalhes'),
@@ -34,5 +41,17 @@ class _PopUpComponentState extends State<PopUpComponent> {
         ),
       ],
     );
+  }
+
+  Future<void> _shareInfo() async {
+    final texto =
+        'Olha esse produto que vi no App: \n\n'
+        '${widget.produto.title} \n'
+        'Por apenas: R\$ ${widget.produto.price} \n\n'
+        'Confira a foto: ${widget.produto.thumbnail}';
+    await SharePlus.instance.share(
+      ShareParams(text: texto, subject: 'Olha esse produto incrível!'),
+    );
+
   }
 }
