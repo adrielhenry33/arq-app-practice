737cda804f63438efc339b0f71106b15127d3b82
Andriel Henry Dias Barbosa <estag202@wetzel.net>
2026-01-06 11:25:09 -0300

    feat: cria o campo de frete

--- lib/app/view/details_view.dart
+++ lib/app/view/details_view.dart
@@ -57,51 +57,123 @@ class _DetailsViewState extends ConsumerState<DetailsView> {
         ],
       ),
 
-      body: Column(
-        children: [
-          SizedBox(
-            height: 350,
-            child: PageView(
-              onPageChanged: (index) {
-                setState(() {
-                  currentIndex = index;
-                });
-              },
-              controller: pagCtrl,
-              children: widget.produto.images.map((urlImage) {
-                return Image.network(
-                  urlImage,
-                  fit: BoxFit.fill,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Center(child: CircularProgressIndicator());
-                  },
-                  errorBuilder: (context, error, stackTrace) {
-                    return Center(child: Icon(Icons.error));
-                  },
-                );
-              }).toList(),
+      body: Padding(
+        padding: EdgeInsets.only(top: 20, right: 20, left: 20, bottom: 10),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 350,
+              child: PageView(
+                onPageChanged: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                controller: pagCtrl,
+                children: widget.produto.images.map((urlImage) {
+                  return Image.network(
+                    urlImage,
+                    fit: BoxFit.fill,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Center(child: CircularProgressIndicator());
+                    },
+                    errorBuilder: (context, error, stackTrace) {
+                      return Center(child: Icon(Icons.error));
+                    },
+                  );
+                }).toList(),
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(widget.produto.images.length, (index) {
-              return AnimatedContainer(
-                duration: Duration(microseconds: 300),
-                curve: Curves.easeOut,
-                margin: EdgeInsets.symmetric(horizontal: 4),
-                height: 8,
-                width: currentIndex == index ? 25 : 8,
-                decoration: BoxDecoration(
-                  color: currentIndex == index
-                      ? Colors.deepPurpleAccent
-                      : Colors.grey[500],
-                  borderRadius: BorderRadius.circular(4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(widget.produto.images.length, (index) {
+                return AnimatedContainer(
+                  duration: Duration(microseconds: 300),
+                  curve: Curves.easeOut,
+                  margin: EdgeInsets.symmetric(horizontal: 4),
+                  height: 8,
+                  width: currentIndex == index ? 25 : 8,
+                  decoration: BoxDecoration(
+                    color: currentIndex == index
+                        ? Colors.deepPurpleAccent
+                        : Colors.grey[500],
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                );
+              }),
+            ),
+
+            SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Calcule Frete e Prazo',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    fontFamily: 'MontSerrat',
+                  ),
                 ),
-              );
-            }),
-          ),
-        ],
+              ],
+            ),
+
+            SizedBox(height: 10),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 50,
+
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Digite seu CEP',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: const BorderSide(
+                              color: Colors.deepPurpleAccent,
+                              width: 2,
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+
+                  MaterialButton(
+                    onPressed: () {},
+                    color: Colors.deepPurpleAccent,
+                    height: 48,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      'Calcular',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
